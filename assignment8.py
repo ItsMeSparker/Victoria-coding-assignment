d2c3f86b5982efb4e48edc0f0b29dd13f988766e
Nico Natipum <146154209+ItsMeSparker@users.noreply.github.com>
2025-08-05 12:27:50 +0700

    Add files via upload

---
+++ assignment8.py
@@ -0,0 +1,460 @@
+import doctest
+import csv
+
+ZERO_LINE_DATA = '/Users/natipumvitayapradit/Downloads/0lines_data.csv'
+NINE_LINE_DATA = '/Users/natipumvitayapradit/Downloads/9lines_data.csv'
+LARGE_DATA = '/Users/natipumvitayapradit/Downloads/large_data.csv'
+#I tried LARGE_DATA with read_file function and it's too large for docstring.
+
+
+# all 2 digit years assumed to be in the 2000s
+START_YEAR = 2000
+
+# represents a Gregorian date as (year, month, day)
+#  where year>=START_YEAR,
+#  month is a valid month, 1-12 to represent January-December
+#  and day is a valid day of the given month and year
+Date = tuple[int, int, int]
+YEAR  = 0
+MONTH = 1
+DAY   = 2
+
+# represents a Netflix show as (show type, title, directors, cast, date added)
+#  where none of the strings are empty strings
+NetflixShow = tuple[str, str, list[str], list[str], Date]
+TYPE      = 0
+TITLE     = 1
+DIRECTORS = 2
+CAST      = 3
+DATE      = 4
+
+# column numbers of data within input csv file
+INPUT_TYPE      = 1
+INPUT_TITLE     = 2
+INPUT_DIRECTORS = 3
+INPUT_CAST      = 4
+INPUT_DATE      = 6
+
+def month_int(month: str) -> int:
+    """
+    This function will be used in create_data function for more simpification.
+    it is just take month as string and return month as number.
+    """
+    if month == 'Jan': return 1
+    elif month == 'Feb': return 2
+    elif month == 'Mar': return 3
+    elif month == 'Apr': return 4
+    elif month == 'May': return 5
+    elif month == 'Jun': return 6
+    elif month == 'Jul': return 7
+    elif month == 'Aug': return 8
+    elif month == 'Sep': return 9
+    elif month == 'Oct': return 10
+    elif month == 'Nov': return 11
+    elif month == 'Dec': return 12
+
+def create_date(Date_str: str) -> list[Date]:
+    """ This function will take date as string and will return the return it
+    in the form of tuple list of 3 integers for further use in this assignment.
+    >>> create_date('10-Jan-18')
+    (2018, 1, 10)
+    >>> create_date('22-Feb-00')
+    (2000, 2, 22)
+    >>> create_date('24-Nov-03')
+    (2003, 11, 24)
+    >>> create_date('01-Nov-24')
+    (2024, 11, 1)
+    >>> create_date('01-Apr-00')
+    (2000, 4, 1)
+    >>> create_date('31-Oct-23')
+    (2023, 10, 31)
+    >>> create_date('25-Dec-17')
+    (2017, 12, 25)
+    """
+    ListDateStr = Date_str.split('-')
+    
+    Day = (int(ListDateStr[0]))
+    Month = (month_int(ListDateStr[1]))
+    Year = (START_YEAR + int(ListDateStr[2]))
+    
+    return (Year, Month, Day)
+
+def read_file(filename: str) -> list[NetflixShow]:
+    """ Reads file at filename into list of NetflixShow format.
+
+    Precondition: filename is in csv format with data in expected columns
+        and contains a header row with the column titles.
+        NOTE: csv = comma separated values where commas delineate columns
+
+    >>> read_file('0lines_data.csv')
+    []
+    
+    >>> read_file('9lines_data.csv')
+    [('Movie', 'SunGanges', ['Valli Bindana'], ['Naseeruddin Shah'], (2019, 11, 15)), \
+('Movie', 'PK', ['Rajkumar Hirani'], ['Aamir Khan', 'Anuskha Sharma', 'Sanjay Dutt', 'Saurabh Shukla', 'Parikshat Sahni', 'Sushant Singh Rajput', 'Boman Irani', 'Rukhsar'], (2018, 9, 6)), \
+('Movie', 'Phobia 2', ['Banjong Pisanthanakun', 'Paween Purikitpanya', 'Songyos Sugmakanan', 'Parkpoom Wongpoom', 'Visute Poolvoralaks'], ['Jirayu La-ongmanee', 'Charlie Trairat', 'Worrawech Danuwong', 'Marsha Wattanapanich', 'Nicole Theriault', 'Chumphorn Thepphithak', 'Gacha Plienwithi', 'Suteerush Channukool', 'Peeratchai Roompol', 'Nattapong Chartpong'], (2018, 9, 5)), \
+('Movie', 'Super Monsters Save Halloween', [], ['Elyse Maloway', 'Vincent Tong', 'Erin Matthews', 'Andrea Libman', 'Alessandro Juliani', 'Nicole Anthony', 'Diana Kaarina', 'Ian James Corlett', 'Britt McKillip', 'Kathleen Barr'], (2018, 10, 5)), ('TV Show', 'First and Last', [], [], (2018, 9, 7)), \
+('Movie', 'Out of Thin Air', ['Dylan Howitt'], [], (2017, 9, 29)), \
+('Movie', 'Shutter', ['Banjong Pisanthanakun', 'Parkpoom Wongpoom'], ['Ananda Everingham', 'Natthaweeranuch Thongmee', 'Achita Sikamana', 'Unnop Chanpaibool', 'Titikarn Tongprasearth', 'Sivagorn Muttamara', 'Chachchaya Chalemphol', 'Kachormsak Naruepatr'], (2018, 9, 5)), \
+('Movie', 'Long Shot', ['Jacob LaMendola'], [], (2017, 9, 29)), ('TV Show', 'FIGHTWORLD', ['Padraic McKinley'], ['Frank Grillo'], (2018, 10, 12))]
+    """
+    # TODO: complete this method according to the documentation
+    result = []
+    file_handler = open(filename, 'r')
+    reader = csv.reader(file_handler)
+    next(reader)
+    
+    for line in reader:
+        Type = line[INPUT_TYPE]
+        Title = line[INPUT_TITLE]
+        if line[INPUT_DIRECTORS]: Director = line[INPUT_DIRECTORS].split(':')
+        else: Director = []
+        if line[INPUT_CAST]: Casts = line[INPUT_CAST].split(':')
+        else: Casts = []
+        Date = create_date(line[INPUT_DATE])
+        result.append((Type, Title, Director, Casts, Date))
+    file_handler.close()
+    return result
+    # Important: DO NOT delete the header row from the csv file,
+    # your function should read the header line and ignore it (do nothing with it)
+    # All files we test your function with will have this header row!
+
+
+
+def get_oldest_titles(show_data: list[NetflixShow]) -> list[str]:
+    """ Returns a list of the titles of NetflixShows in show_data
+    with the oldest added date
+
+    >>> shows_unique_dates = [\
+    ('Movie', 'Super Monsters Save Halloween', [],\
+    ['Elyse Maloway', 'Vincent Tong', 'Erin Matthews', 'Andrea Libman',\
+    'Alessandro Juliani', 'Nicole Anthony', 'Diana Kaarina', 'Ian James Corlett',\
+    'Britt McKillip', 'Kathleen Barr'], (2018, 10, 5)),\
+    ('TV Show', 'First and Last', [], [], (2018, 9, 7)),\
+    ('Movie', 'Out of Thin Air', ['Dylan Howitt'], [], (2017, 9, 29))]
+
+    >>> shows_duplicate_oldest_date = [\
+    ('Movie', 'Super Monsters Save Halloween', [],\
+    ['Elyse Maloway', 'Vincent Tong', 'Erin Matthews', 'Andrea Libman',\
+    'Alessandro Juliani', 'Nicole Anthony', 'Diana Kaarina',\
+    'Ian James Corlett', 'Britt McKillip', 'Kathleen Barr'], (2017, 9, 29)),\
+    ('TV Show', 'First and Last', [], [], (2018, 9, 7)),\
+    ('Movie', 'Out of Thin Air', ['Dylan Howitt'], [], (2017, 9, 29))]
+
+    >>> get_oldest_titles([])
+    []
+    >>> get_oldest_titles(shows_unique_dates)
+    ['Out of Thin Air']
+    >>> get_oldest_titles(shows_duplicate_oldest_date)
+    ['Super Monsters Save Halloween', 'Out of Thin Air']
+    """
+    if not show_data:
+        return []
+    
+    OldestDate = show_data[0][DATE]
+    OldestTitle = [show_data[0][TITLE]] #Initialize being a list
+    
+    '''
+    I find out that we can just compare like this, by don't have to access
+    through that element inside by ourselve
+    >>> (2018, 9, 7) > (2017, 9, 29)
+    True
+    >>> (2018, 9, 7) < (2017, 9, 29)
+    False
+    >>> (2018, 9, 7) > (2018, 9, 6)
+    True
+    >>> (2018, 5, 6) < (2018, 9, 6)
+    True
+    >>> (2018, 10, 1) < (2018, 9, 28)
+    False
+    >>> (2018, 10, 1) > (2018, 9, 28)
+    True
+    '''
+    
+    for Show in show_data[1:]: #Start the loop with movie 1
+        CurrentDate = Show[DATE]
+        if CurrentDate < OldestDate:
+            OldestDate = CurrentDate
+            OldestTitle = [Show[TITLE]] #Change into the list
+        elif CurrentDate == OldestDate: #If date equal
+            OldestTitle.append(Show[TITLE]) #Append into the list
+
+    return OldestTitle
+
+
+def get_actors_in_most_shows(shows: list[NetflixShow]) -> list[str]:
+    """ Returns a sorted list of actors that are in the casts of the most shows
+
+    >>> l_unique_casts = [\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],\
+    ['Hugh Bonneville', 'Om Puri', 'Lily Travers'], (2017, 12, 12)),\
+    ('Movie', 'Superbad', ['Greg Mottola'], ['Michael Cera'], (2019, 9, 1)), \
+    ('TV Show', 'Maniac', [], ['Emma Stone'], (2018, 9, 21)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], ['Paresh Rawal'], (2019, 12, 31))]
+
+    >>> one_actor_in_multiple_casts = [\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],\
+    ['Hugh Bonneville', 'Om Puri', 'Lily Travers'], (2017, 12, 12)),\
+    ('Movie', 'Superbad', ['Greg Mottola'], ['Jonah Hill', 'Michael Cera'],\
+    (2019, 9, 1)),\
+    ('TV Show', 'Maniac', [], ['Emma Stone', 'Jonah Hill', 'Justin Theroux'], \
+    (2018, 9, 21)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], ['Paresh Rawal'], \
+    (2019, 12, 31))]
+
+    >>> actors_in_multiple_casts = [\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],\
+    ['Hugh Bonneville', 'Om Puri', 'Lily Travers'], (2017, 12, 12)),\
+    ('Movie', 'Superbad', ['Greg Mottola'], ['Jonah Hill', 'Michael Cera'],\
+    (2019, 9, 1)),\
+    ('TV Show', 'Maniac', [], ['Emma Stone', 'Jonah Hill', 'Justin Theroux'], \
+    (2018, 9, 21)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], ['Paresh Rawal', 'Om Puri'], \
+    (2019, 12, 31))]
+
+    >>> get_actors_in_most_shows([])
+    []
+
+    >>> get_actors_in_most_shows(l_unique_casts) # doctest: +NORMALIZE_WHITESPACE
+    ['Emma Stone', 'Hugh Bonneville', 'Lily Travers', 'Michael Cera', \
+    'Om Puri', 'Paresh Rawal']
+
+    >>> get_actors_in_most_shows(one_actor_in_multiple_casts)
+    ['Jonah Hill']
+
+    >>> get_actors_in_most_shows(actors_in_multiple_casts)
+    ['Jonah Hill', 'Om Puri']
+    """
+    #Handle the case of blank parameter
+    if not shows:
+        return []
+    
+    #Count the appearance of actors through dictionary
+    ActorCount = {}
+    for Show in shows:
+        Cast = Show[CAST]
+        for Actor in Cast:
+            if Actor in ActorCount: #If an actor already has name on dictionry
+                ActorCount[Actor] += 1
+            else: #if an actor just first appear
+                ActorCount[Actor] = 1
+    
+    #Finding the MostAppearances in dictionary
+    MostAppearances = 0
+    for Count in ActorCount.values():
+        if Count > MostAppearances:
+            MostAppearances = Count
+    
+    #After that I use this way to handle multiple most appearance
+    Result = []
+    for Actor, Appearace in ActorCount.items():
+        if Appearace == MostAppearances:
+            Result.append(Actor)
+    Result.sort()
+    return Result
+
+def is_substring(Subword: str, Word: str) -> bool:
+    """
+    This function is built for indicate if subword is in the word or not
+    >>> is_substring('tIm', 'TimHolton')
+    True
+    >>> is_substring('FEE', 'Coffee')
+    True
+    >>> is_substring('kc', 'Brick')
+    False
+    >>> is_substring('', 'Football')
+    True
+    """
+    SubLength = len(Subword)
+    WordLength = len(Word)
+    
+    for Letter in range(WordLength - SubLength + 1):
+        #To fit the size of a subword and prevent index of of bound
+        if Word[Letter:Letter + SubLength].upper() == Subword.upper():
+            return True
+    return False
+
+
+def get_shows_with_search_terms(show_data: list[NetflixShow], terms: list[str]
+                                 ) -> list[NetflixShow]:
+    """ returns a list of only those NetflixShow elements in show_data
+    that contain any of the given terms in the title.
+    If terms is empty, all elements in show_data will be included in the returned list.
+    Matching of terms ignores case ('roAD' is found in 'Road to Sangam') and
+    matches on substrings ('Sang' is found in 'Road to Sangam')
+
+    Precondition: the strings in terms are not empty strings
+
+    >>> movies = [\
+    ('Movie', 'Rang De Basanti', ['Rakeysh Omprakash Mehra'], \
+     ['Aamir Khan', 'Siddharth', 'Atul Kulkarni', 'Sharman Joshi', 'Kunal Kapoor',  \
+      'Alice Patten', 'Soha Ali Khan', 'Waheeda Rehman', 'Kiron Kher', 'Om Puri', \
+      'Anupam Kher', 'Madhavan'],  \
+     (2018, 8, 2)),\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],  \
+     ['Hugh Bonneville', 'Gillian Anderson', 'Manish Dayal', 'Huma Qureshi',  \
+      'Michael Gambon', 'David Hayman', 'Simon Callow', 'Denzil Smith',  \
+      'Neeraj Kabi', 'Tanveer Ghani', 'Om Puri', 'Lily Travers'],  \
+     (2017, 12, 12)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], \
+      ['Paresh Rawal', 'Om Puri', 'Pavan Malhotra', 'Javed Sheikh', \
+       'Swati Chitnis', 'Masood Akhtar', 'Sudhir Nema', 'Rakesh Srivastava'], \
+      (2019, 12, 31))]
+
+    >>> terms1 = ['House']
+    >>> terms1_wrong_case = ['hoUSe']
+
+    >>> terms_subword = ['Sang']
+
+    >>> terms2 = ['House', 'Road', 'Basanti']
+    >>> terms2_wrong_case = ['house', 'ROAD', 'bAsanti']
+
+    >>> get_shows_with_search_terms([], [])
+    []
+
+    >>> get_shows_with_search_terms(movies, []) # doctest: +NORMALIZE_WHITESPACE
+    [('Movie', 'Rang De Basanti', ['Rakeysh Omprakash Mehra'], \
+      ['Aamir Khan', 'Siddharth', 'Atul Kulkarni', 'Sharman Joshi', 'Kunal Kapoor',  \
+       'Alice Patten', 'Soha Ali Khan', 'Waheeda Rehman', 'Kiron Kher', 'Om Puri', \
+       'Anupam Kher', 'Madhavan'],  \
+      (2018, 8, 2)),\
+     ('Movie', "Viceroy's House", ['Gurinder Chadha'],  \
+      ['Hugh Bonneville', 'Gillian Anderson', 'Manish Dayal', 'Huma Qureshi',  \
+       'Michael Gambon', 'David Hayman', 'Simon Callow', 'Denzil Smith',  \
+       'Neeraj Kabi', 'Tanveer Ghani', 'Om Puri', 'Lily Travers'],  \
+      (2017, 12, 12)),\
+     ('Movie', 'Road to Sangam', ['Amit Rai'], \
+       ['Paresh Rawal', 'Om Puri', 'Pavan Malhotra', 'Javed Sheikh', \
+        'Swati Chitnis', 'Masood Akhtar', 'Sudhir Nema', 'Rakesh Srivastava'], \
+       (2019, 12, 31))]
+    >>> get_shows_with_search_terms([], terms1)
+    []
+
+    >>> get_shows_with_search_terms(movies, terms1) # doctest: +NORMALIZE_WHITESPACE
+    [('Movie', "Viceroy's House", ['Gurinder Chadha'], 
+      ['Hugh Bonneville', 'Gillian Anderson', 'Manish Dayal', 'Huma Qureshi', \
+       'Michael Gambon', 'David Hayman', 'Simon Callow', 'Denzil Smith', \
+       'Neeraj Kabi', 'Tanveer Ghani', 'Om Puri', 'Lily Travers'], \
+      (2017, 12, 12))]
+
+    >>> get_shows_with_search_terms(movies, terms1_wrong_case) # doctest: +NORMALIZE_WHITESPACE
+    [('Movie', "Viceroy's House", ['Gurinder Chadha'], \
+      ['Hugh Bonneville', 'Gillian Anderson', 'Manish Dayal', 'Huma Qureshi', \
+       'Michael Gambon', 'David Hayman', 'Simon Callow', 'Denzil Smith', \
+       'Neeraj Kabi', 'Tanveer Ghani', 'Om Puri', 'Lily Travers'], \
+      (2017, 12, 12))]
+
+    >>> get_shows_with_search_terms(movies, terms_subword) # doctest: +NORMALIZE_WHITESPACE
+    [('Movie', 'Road to Sangam', ['Amit Rai'], \
+      ['Paresh Rawal', 'Om Puri', 'Pavan Malhotra', 'Javed Sheikh', \
+       'Swati Chitnis', 'Masood Akhtar', 'Sudhir Nema', 'Rakesh Srivastava'], \
+      (2019, 12, 31))]
+
+    >>> get_shows_with_search_terms(movies, terms2) # doctest: +NORMALIZE_WHITESPACE
+    [('Movie', 'Rang De Basanti', ['Rakeysh Omprakash Mehra'], \
+      ['Aamir Khan', 'Siddharth', 'Atul Kulkarni', 'Sharman Joshi', \
+       'Kunal Kapoor', 'Alice Patten', 'Soha Ali Khan', 'Waheeda Rehman', \
+       'Kiron Kher', 'Om Puri', 'Anupam Kher', 'Madhavan'], 
+      (2018, 8, 2)), \
+     ('Movie', "Viceroy's House", ['Gurinder Chadha'], \
+      ['Hugh Bonneville', 'Gillian Anderson', 'Manish Dayal', 'Huma Qureshi', \
+        'Michael Gambon', 'David Hayman', 'Simon Callow', 'Denzil Smith', \
+        'Neeraj Kabi', 'Tanveer Ghani', 'Om Puri', 'Lily Travers'], \
+       (2017, 12, 12)), \
+      ('Movie', 'Road to Sangam', ['Amit Rai'], \
+       ['Paresh Rawal', 'Om Puri', 'Pavan Malhotra', 'Javed Sheikh', \
+        'Swati Chitnis', 'Masood Akhtar', 'Sudhir Nema', 'Rakesh Srivastava'], \
+       (2019, 12, 31))]
+
+    >>> get_shows_with_search_terms(movies, terms2_wrong_case) # doctest: +NORMALIZE_WHITESPACE
+    [('Movie', 'Rang De Basanti', ['Rakeysh Omprakash Mehra'], \
+      ['Aamir Khan', 'Siddharth', 'Atul Kulkarni', 'Sharman Joshi', \
+       'Kunal Kapoor', 'Alice Patten', 'Soha Ali Khan', 'Waheeda Rehman', \
+       'Kiron Kher', 'Om Puri', 'Anupam Kher', 'Madhavan'], \
+      (2018, 8, 2)), \
+     ('Movie', "Viceroy's House", ['Gurinder Chadha'], \
+      ['Hugh Bonneville', 'Gillian Anderson', 'Manish Dayal', 'Huma Qureshi', \
+       'Michael Gambon', 'David Hayman', 'Simon Callow', 'Denzil Smith', \
+       'Neeraj Kabi', 'Tanveer Ghani', 'Om Puri', 'Lily Travers'], \
+      (2017, 12, 12)), \
+     ('Movie', 'Road to Sangam', ['Amit Rai'], \
+      ['Paresh Rawal', 'Om Puri', 'Pavan Malhotra', 'Javed Sheikh', \
+       'Swati Chitnis', 'Masood Akhtar', 'Sudhir Nema', 'Rakesh Srivastava'], \
+      (2019, 12, 31))]
+    """
+    
+    if not terms:
+        return show_data
+    
+    Result = []
+    for Show in show_data:
+        for KeyWord in terms:
+            if KeyWord.upper() == Show[TITLE].upper():
+                Result.append(Show)
+                break
+            elif is_substring(KeyWord, Show[TITLE]):
+                Result.append(Show)
+                break
+    return Result
+
+
+
+def query(show_data: list[NetflixShow]) -> list[str]:
+    """
+    Returns a sorted list of only the show titles from show_data
+    that are acted in by the 'most popular' actors
+    where the 'most popular' is defined as the actors in the most shows.
+
+    >>> l_unique_casts = [\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],\
+    ['Hugh Bonneville', 'Om Puri', 'Lily Travers'], (2017, 12, 12)),\
+    ('Movie', 'Superbad', ['Greg Mottola'], ['Michael Cera'], (2019, 9, 1)), \
+    ('TV Show', 'Maniac', [], ['Emma Stone'], (2018, 9, 21)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], ['Paresh Rawal'], (2019, 12, 31))]
+    
+    >>> one_actor_in_multiple_casts = [\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],\
+    ['Hugh Bonneville', 'Om Puri', 'Lily Travers'], (2017, 12, 12)),\
+    ('Movie', 'Superbad', ['Greg Mottola'], ['Jonah Hill', 'Michael Cera'],\
+    (2019, 9, 1)),\
+    ('TV Show', 'Maniac', [], ['Emma Stone', 'Jonah Hill', 'Justin Theroux'], \
+    (2018, 9, 21)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], ['Paresh Rawal'], \
+    (2019, 12, 31))]
+    
+    >>> actors_in_multiple_casts = [\
+    ('Movie', "Viceroy's House", ['Gurinder Chadha'],\
+    ['Hugh Bonneville', 'Om Puri', 'Lily Travers'], (2017, 12, 12)),\
+    ('Movie', 'Superbad', ['Greg Mottola'], ['Jonah Hill', 'Michael Cera'],\
+    (2019, 9, 1)),\
+    ('TV Show', 'Maniac', [], ['Emma Stone', 'Jonah Hill', 'Justin Theroux'], \
+    (2018, 9, 21)),\
+    ('Movie', 'Road to Sangam', ['Amit Rai'], ['Paresh Rawal', 'Om Puri'], \
+    (2019, 12, 31))]
+    
+    >>> query([])
+    []
+    
+    >>> query(l_unique_casts)
+    ['Maniac', 'Road to Sangam', 'Superbad', "Viceroy's House"]
+    
+    >>> query(one_actor_in_multiple_casts)
+    ['Maniac', 'Superbad']
+
+    >>> query(actors_in_multiple_casts)
+    ['Maniac', 'Road to Sangam', 'Superbad', "Viceroy's House"]
+    """
+    MostFamousActor = get_actors_in_most_shows(show_data)
+    Result = []
+   
+    for Show in show_data:
+        Popular = False #I use this as a flag to add movie to list only one time
+        for Actor in Show[CAST]:
+            if Actor in MostFamousActor:
+                Popular = True
+        if Popular: Result.append(Show[TITLE])
+    
+    Result.sort()
+    return Result
+    
+    # TODO: complete this function according to the documentation
