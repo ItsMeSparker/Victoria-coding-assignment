50fd5cdff24e50b68ed2480176301f25e3191d48
Nico Natipum <146154209+ItsMeSparker@users.noreply.github.com>
2025-08-11 15:44:41 +0700

    Add files via upload

---
+++ assignment8Real.py
@@ -0,0 +1,19 @@
+ZERO_LINE_DATA = '/Users/natipumvitayapradit/Downloads/0lines_data.csv'
+NINE_LINE_DATA = '/Users/natipumvitayapradit/Downloads/9lines_data.csv'
+LARGE_DATA = '/Users/natipumvitayapradit/Downloads/large_data.csv'
+
+
+def read_file():
+    print()
+    
+def get_oldest_titles():
+    print()
+    
+def get_actors_in_most_shows():
+    print()
+
+def get_shows_with_search_terms():
+    print()
+    
+def query():
+    print()
